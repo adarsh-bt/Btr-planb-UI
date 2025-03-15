3c841ad85b13dc5481d30dad9720d6e865ffe83c
adarsh-bt <adarsh.a@duk.ac.in>
2025-03-15 22:05:00 +0530

    add Directorate,district,taluk form and table

--- src/pages/functional-components/approvels/directorate_users.jsx
+++ src/pages/functional-components/approvels/directorate_users.jsx
@@ -6,7 +6,7 @@ import DataTable from 'react-data-table-component';
 import Swal from "sweetalert2";
 
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import Directorate from './directorate_users';
+
 
 import {
     Typography,
@@ -28,6 +28,7 @@ import {
     Radio,
   } from '@mui/material';
 import functionalservice from '../functionalservice';
+
 
 const columns = (handleEdit) => [
     { name: 'SL. NO', selector:(row, index) => index + 1, sortable: true },
@@ -105,7 +106,7 @@ function a11yProps(index) {
   };
 }
 
-export default function BasicTabs() {
+export default function Directorate() {
   const [value, setValue] = React.useState(0);
 
 
@@ -279,19 +280,11 @@ const filteredData = userList.filter((item) =>
 console.log("filtr",filterText.toLowerCase())
 return (
 
-    <Box style={{background:'white'}}>
-    <Typography variant='h4' p={1}>Approvals </Typography>
-    <hr></hr>
+ 
+   
     <Box sx={{ width: '100%' }}>
-      <Box sx={{borderColor: 'Boxider',marginLeft:'1rem' ,justifyContent: 'center',alignItems:'center'}}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example"  indicatorColor="primary" >
-          <Tab label="District Level Users" {...a11yProps(0)} />
-          <Tab label="Taluk Level Users" {...a11yProps(1)} />
-          <Tab label="Directorate User Request" {...a11yProps(2)} />
-          <Tab label="Others Request" {...a11yProps(3)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
+     
+   
 
 
       
@@ -356,267 +349,7 @@ return (
 
 
 {/* Modal for District Users */}
-
-
-    
-
-{/* Modal for Taluk Users */}
-{/* <Dialog open={openModal2} onClose={handleCloseModal} maxWidth="sm" fullWidth> */}
-<Dialog maxWidth="sm" fullWidth>
-
-  <DialogTitle
-    variant="h4"
-    style={{
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center",
-      borderBottom: "2px solid #f0f0f0",
-      paddingBottom: "10px",
-      background: '#04255e',
-    }}
-  >
-    Taluk User Request
-  </DialogTitle>
-  <DialogContent style={{ padding: "20px", backgroundColor: "#fafafa" }}>
-    {selectedRow && (
-      <DialogContentText>
-        <Stack
-          spacing={2}
-          style={{
-            fontSize: "14px",
-            color: "#333",
-            backgroundColor: "#fff",
-            padding: "20px",
-            borderRadius: "8px",
-            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          {[{ label: "Name", value: selectedRow.name },
-            { label: "Date Of birth", value: selectedRow.dateOfBirth },
-            { label: "Email", value: selectedRow.email },
-            { label: "Phone Number", value: selectedRow.mobileNumber },
-            { label: "Date of Joining", value: selectedRow.dateOfJoining },
-            { label: "Pen", value: selectedRow.penNumber },
-            { label: "Office", value: selectedRow.officeToJoining }
-          ].map((field, index) => (
-            <Box
-              key={index}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "10px",
-              }}
-            >
-              <Box
-                style={{
-                  textAlign: "right",
-                  marginRight: "8px",
-                  width: "40%",
-                  fontWeight: "bold",
-                  color: "#555",
-                }}
-              >
-                {field.label}:
-              </Box>
-              <Box
-                style={{
-                  textAlign: "left",
-                  width: "60%",
-                  backgroundColor: "#f9f9f9",
-                  padding: "5px 10px",
-                  borderRadius: "4px",
-                  boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.1)",
-                }}
-              >
-                {field.value}
-              </Box>
-            </Box>
-          ))}
-        </Stack>
-
-        {/* Category and Duties Dropdowns */}
-        <Box
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: "20px",
-            textAlign: "center",
-            padding: "10px",
-            border: "1px solid #f0f0f0",
-            borderRadius: "8px",
-            backgroundColor: "#fff",
-            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-        </Box>
-
-        {/* Status Radio Buttons */}
-        <Box
-          style={{
-            marginTop: "20px",
-            textAlign: "center",
-            padding: "10px",
-            border: "1px solid #f0f0f0",
-            borderRadius: "8px",
-            backgroundColor: "#fff",
-            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <strong>Status:</strong>
-        <FormGroup row style={{ justifyContent: "center", marginTop: "8px" }}>
-        
-  <FormControlLabel
-    sx={{ color: 'success.main' }}
-    control={
-      <Radio
-        checked={radioState === "approved"}
-        onChange={() => handleRadioChange("approved")}
-        disabled={selectedRow.approvalStatus === "Approved"} // Make "Approved" radio button read-only
-      />
-    }
-    label="Approved"
-  />
-  <FormControlLabel
-    sx={{ color: 'warning.main' }}
-    control={
-      <Radio
-        checked={radioState === "marked"}
-        onChange={() => handleRadioChange("marked")}
-        disabled={selectedRow.approvalStatus === "Approved"} 
-      />
-    }
-    label="Marked"
-  />
-  <FormControlLabel
-    sx={{ color: 'error.main' }}
-    control={
-      <Radio
-        checked={radioState === "rejected"}
-        onChange={() => handleRadioChange("rejected")}
-        disabled={selectedRow.approvalStatus === "Approved"} 
-      />
-    }
-    label="Rejected"
-  />
-</FormGroup>
-{selectedRow.approvalStatus === "Approved" ? <Typography sx={{ color: 'primary.main' }}>Already Approved only View</Typography> : null}
-        </Box>
-
-        {/* Remarks Textbox if Rejected */}
-        {(radioState === "rejected" || radioState === "marked") && (
-          <Box
-            style={{
-              marginTop: "16px",
-              textAlign: "center",
-              padding: "10px",
-              border: "1px solid #f0f0f0",
-              borderRadius: "8px",
-              backgroundColor: "#fff",
-              boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            <TextField
-              label="Remarks"
-              type="text"
-              fullWidth
-              variant="outlined"
-              style={{ fontSize: "14px" }}
-              value={remarks}  // Bind to state
-              onChange={(e) => setRemarks(e.target.value)}
-            />
-          </Box>
-        )}
-      </DialogContentText>
-    )}
-  </DialogContent>
-  <DialogActions style={{ justifyContent: "center" }}>
-    <Button onClick={handleCloseModal} color="secondary" variant="outlined">
-      Close
-    </Button>
-    <Button
-      onClick={handleSaveChanges}
-      color="primary"
-      variant="contained"
-      sx={{ background: '#04255e' }}
-      disabled={!isSaveEnabled}
-    >
-      Save Changes
-    </Button>
-  </DialogActions>
-</Dialog>
-
-
-      </CustomTabPanel>
-      {/* Taluk Table */}
-      <CustomTabPanel value={value} index={1}>
-      <Paper elevation={3} style={{  padding: '10px',}}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
-          <Typography variant="h5" style={{ fontWeight: 'bold', color: '#333' }}>
-          Taluk User Request 
-          </Typography>
-          <TextField
-            label="Filter"
-            variant="outlined"
-            value={filterText}
-            onChange={handleFilterChange}
-            size="small"
-            style={{ width: '200px' }}
-          />
-        </Stack>
-      </Paper>
-
-
-      <DataTable
-      
-        columns={columns(handleEdit)} // Pass handleEdit to columns function
-        data={filteredData}
-        pagination
-        paginationComponentOptions={{
-          rowsPerPageText: 'Rows per page',
-          rangeSeparatorText: 'of',
-          selectAllRowsItemText: 'All',
-          selectAllRowsItem: 'Select All',
-        }}
-        customStyles={{
-       
-          headCells: {
-            style: {
-                fontSize:'.8rem',
-              backgroundColor: '#04255e', // Header background color
-              color: '#fff', // Header text color
-              fontWeight: 'bold', // Bold header text
-              borderBottom: '2px solid black', // Classic border style
-           
-            },
-          },
-          cells: {
-            style: {
-              backgroundColor: '',
-              borderBottom: '1px solid white', // Light bottom border for rows
-              color: '#333', // Darker text color for better readability
-             
-            },
-          },
-          pagination: {
-            style: {
-              color: '#04255e', // Change pagination symbols to blue
-             
-              alignItems:'center',
-              justifyContent:'center'
-            },
-          },
-        }}
-      />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-      <Directorate></Directorate>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={3}>
-        Other Requests
-      </CustomTabPanel>
-    </Box>
-    <Dialog open={openModal} onClose={handleCloseModal} maxWidth="sm" fullWidth>
+<Dialog open={openModal} onClose={handleCloseModal} maxWidth="sm" fullWidth>
   <DialogTitle
     variant="h4"
     style={{
@@ -869,6 +602,9 @@ return (
     </Button>
   </DialogActions>
 </Dialog>
+    
+     
+  
     </Box>
   );
 }
