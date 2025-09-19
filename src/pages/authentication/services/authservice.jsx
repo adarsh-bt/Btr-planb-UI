1b10205ff2c355abcf68f2585f5c64ea661e3c4e
Reshmaduk <reshma.mb@duk.ac.in>
2025-09-19 12:23:30 +0530

    pull from adarsh01 dated 19/9/25

--- src/pages/authentication/services/authservice.jsx
+++ src/pages/authentication/services/authservice.jsx
@@ -2,35 +2,29 @@ import axios from 'axios';
 import { InvalidTokenError, jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 import { encryptData, decryptData } from './encryptionUtils';
-
 import mainapi from 'api/mainapi';
-
 class authservice {
   static BASE_URL = mainapi.BASE_URL;
   //   static BASE_URL = "https://9a89-103-149-159-190.ngrok-free.app";
-
   static async login(userLogin) {
     try {
-      
       const encrypted = encryptData(JSON.stringify(userLogin));
       console.log("user login ",encrypted)
-
+      
       const response = await axios.post(`${authservice.BASE_URL}/user-access/api/login`, encrypted, {
         headers: {
           'Content-Type': 'text/plain'
         }
       });
-
+     
       // Decrypt the response here
       const decryptedJson = decryptData(response.data);
       const responseData = JSON.parse(decryptedJson);
-
       // localStorage.setItem('pression', response.payload.schmes);
       console.log('pression', response.payload);
       // Store token/user
       localStorage.setItem('token', responseData.payload.token);
       localStorage.setItem('user', responseData.payload.username);
-
       console.log('>> >>>>> >>>>>> ', responseData);
       return responseData;
     } catch (err) {
@@ -42,7 +36,6 @@ try {
 } catch (decryptionError) {
   decryptedError = 'Unknown encrypted error from backend';
 }
-
 console.log("err ",decryptedError.message)
     return {
       message: decryptedError || err.response.data.message || 'Unknown error from backend'
@@ -57,9 +50,7 @@ console.log("err ",decryptedError.message)
     };
   }
 }
-
   }
-
   static async fetchPermissions(token) {
     try {
       const response = await axios.get(`${authservice.BASE_URL}/user-access/user-state/userpremissions`, {
@@ -76,7 +67,6 @@ console.log("err ",decryptedError.message)
       throw new Error((err.response && err.response.data && err.response.data.message) || 'Failed to fetch permissions');
     }
   }
-
   static async registration(userData) {
     try {
       console.log('userdataregister > ', userData);
@@ -94,7 +84,6 @@ console.log("err ",decryptedError.message)
       };
     }
   }
-
   static async email_verification(username) {
     try {
       const encrypted = encryptData(JSON.stringify(username));
@@ -117,7 +106,6 @@ console.log("err ",decryptedError.message)
       };
     }
   }
-
   static async verify_otp(userData) {
     try {
       const encrypted = encryptData(JSON.stringify(userData));
@@ -126,7 +114,6 @@ console.log("err ",decryptedError.message)
           'Content-Type': 'text/plain'
         }
       });
-
       const decryptedJson = decryptData(response.data);
       const responseData = JSON.parse(decryptedJson);
       return responseData;
@@ -134,7 +121,6 @@ console.log("err ",decryptedError.message)
       throw err;
     }
   }
-
   static async password_reset(userData) {
     try {
       const encrypted = encryptData(JSON.stringify(userData));
@@ -151,19 +137,16 @@ console.log("err ",decryptedError.message)
       throw err;
     }
   }
-
   // Checker
-
 static async logout(navigate) {
   try {
     const token = localStorage.getItem('token');
-
     const response = await axios.post(`${authservice.BASE_URL}/user-access/api/logout`, null, {
       headers: {
         'Authorization': `Bearer ${token}`, // or handled via cookie if not using token in header
         'Content-Type': 'application/json'
       },
-      withCredentials: true 
+      withCredentials: true
     });
     console.log("api   ????? ",authservice.BASE_URL);
     if (response.status === 200) {
@@ -172,7 +155,6 @@ static async logout(navigate) {
   localStorage.removeItem('activeZone');
   navigate('/login');
 }
-
     return response.data;
   } catch (error) {
     if (error.response) {
@@ -182,27 +164,21 @@ static async logout(navigate) {
     }
   }
 }
-
   // static logout(navigate) {
   //   localStorage.removeItem('token');
   //   localStorage.removeItem('user');
-
   //   navigate('/login');
   // }
-
   static userid() {
     const token = localStorage.getItem('token');
     const decodedToken = jwtDecode(token);
     return decodedToken.sub;
   }
-
   static getrole() {
     const token = localStorage.getItem('token');
     const decodedToken = jwtDecode(token);
-
     return decodedToken.roles;
   }
-
   static getzone() {
     try{
       const zoneId = localStorage.getItem('activeZone');
@@ -211,34 +187,26 @@ static async logout(navigate) {
       console.error('Error decoding token:', error);
     }
   }
-
   static getusername() {
     const user_name = localStorage.getItem('user');
-   
     return user_name;
   }
-
   static gettoken() {
     return localStorage.getItem('token');
   }
-
   static hasAllowedRole() {
     const token = localStorage.getItem('token');
     if (!token) return false;
-
     try {
       const decoded = jwtDecode(token);
       let userRoles = decoded.roles;
-
       // Normalize roles into an array
       if (typeof userRoles === 'string') {
         userRoles = [userRoles];
       } else if (!Array.isArray(userRoles)) {
         return false; // Unexpected format
       }
-
       const allowedRoles = new Set(['Taluk Level Approver', 'District Level Approver', 'Super Admin', 'State Level Approver', 'IT Admin']);
-
       return userRoles.some((role) => allowedRoles.has(role));
     } catch (error) {
       console.error('Invalid token:', error);
@@ -246,5 +214,4 @@ static async logout(navigate) {
     }
   }
 }
-
 export default authservice;
