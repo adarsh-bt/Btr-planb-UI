185a9952e65b88e2821e29eeaeca9aee46ac0bde
robinsamuelroy576 <robin.roy@duk.ac.in>
2025-12-19 11:27:29 +0530

    saved after long

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
       {/* <UserAccessProvider> */}
         <ScrollTop>
           <RouterProvider router={router} />
-        </ScrollTop>
+      </ScrollTop>
       {/* </UserAccessProvider> */}
     </ThemeCustomization>
   );
