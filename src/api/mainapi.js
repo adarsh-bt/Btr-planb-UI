dfe1fa4c9f8aeeb411cde8564ad2144aab713e90
robinsamuelroy576 <robin.roy@duk.ac.in>
2025-12-22 14:11:19 +0530

    saved before pull

--- src/api/mainapi.js
+++ src/api/mainapi.js
@@ -5,23 +5,23 @@ class mainapi {
 
     // static BASE_URL = 'http://65.2.110.20:8080';
   // static BASE_URL = 'http://3.108.98.206:8080';
-  // static BASE_URL = 'http://localhost:8080';
-  static BASE_URL = 'http://103.156.188.49:8080';
+  static BASE_URL = 'http://localhost:8080';
+  // static BASE_URL = 'http://103.156.188.49:8080';
 
   // User API
   // static USER_API = 'http://3.108.98.206:8080';
-  // static USER_API = 'http://localhost:8080';
-  static USER_API = 'http://103.156.188.49:8080';
+  static USER_API = 'http://localhost:8080';
+  // static USER_API = 'http://103.156.188.49:8080';
 
   // BTR API
   // static BTR_API = 'http://3.108.98.206:8080';
-  // static BTR_API = 'http://localhost:8080';
-  static BTR_API = 'http://103.156.188.49:8080';
+  static BTR_API = 'http://localhost:8080';
+  // static BTR_API = 'http://103.156.188.49:8080';
 
   //Form API
   // static FORM_API = 'http://3.108.98.206:8080';
-  // static FORM_API = 'http://localhost:8080';
-  static FORM_API = 'http://103.156.188.49:8080';
+  static FORM_API = 'http://localhost:8080';
+  // static FORM_API = 'http://103.156.188.49:8080';
 }
 
 export default mainapi;
